9e32554481d1b0948282c025f702e22b3dc2b595
Sarwar-Putul <sarwarputul@gmail.com>
2021-02-03 02:16:10 +0600

    true false done

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// //Falsy javascript নিচের গুলকে false মনে করে ।
+// false
+// 0
+// ""
+// undified
+// null
+// NaN
+// //Truthy javascript নিচের গুলকে true মনে করে। 
+// '0'
+// ' '
+// []
+// "false"
+// any number but not (0)
+
+const age = 0;
+if (age || name == 0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
