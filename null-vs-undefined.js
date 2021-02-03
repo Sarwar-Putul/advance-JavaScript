e837a83919b6faaf8410902f6b0a75a1174be64c
Sarwar-Putul <sarwarputul@gmail.com>
2021-02-03 11:32:06 +0600

    null vs undefined viva

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,21 +1,29 @@
 let pakhi;
-
+-- -- -- -- -- -- -- -- -- -- -- -- -
 function add(num1, num2) {
     console.log(num1 + num2);
     return
 }
+-- -- -- -- -- -- -- -- -- -- -
+
+function doSomething(x, y) {
+
+    console.log(y);
+}
+doSomething(32);
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 function add(num1, num2) {
     console.log(num1 + num2);
 }
-
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 const premik = { name: 'smart dude', phone: 345678 };
 console.log(premik.gf);
-
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 let fun = undefined;
 console.log(fun);
-
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 let ages = [2, 5, 9];
 console.log(ages[11];)
